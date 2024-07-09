211a9f3bcefc9759999ed06062ae665ff614a7d8
Jinson25 <am987406@gmail.com>
2024-07-09 10:51:07 -0500

    feat: Add proposals list component to home page

--- Frontend/src/app/pages/client/home-page/home-page.component.ts
+++ Frontend/src/app/pages/client/home-page/home-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from '../../../components/product-list/product-list.component';
 import { ProfileInfoCardComponent } from '../../../components/profile-info-card/profile-info-card.component';
+import { ProposalsListComponent } from '../../../components/proposals-list/proposals-list.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule, ProductListComponent, ProfileInfoCardComponent],
+  imports: [CommonModule, ProductListComponent, ProfileInfoCardComponent, ProposalsListComponent],
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
