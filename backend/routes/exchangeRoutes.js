586bbb9a2c04112a7de258fd1aa75341ac547dd6
Helton Quezada <info.johnjairo@gmail.com>
2024-07-29 16:12:35 -0500

    cambios

--- backend/routes/exchangeRoutes.js
+++ backend/routes/exchangeRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { proposeExchange, getReceivedExchanges, getSentExchanges, updateExchangeStatus, getExchangeById, uploadReceipt } = require('../controllers/exchangeController');
+const { proposeExchange, getReceivedExchanges, getSentExchanges, updateExchangeStatus, getExchangeById, uploadReceipt, getAllExchanges, getCompletedExchanges } = require('../controllers/exchangeController');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
 const multer = require('multer');
@@ -10,6 +10,8 @@ router.get('/received', authMiddleware, getReceivedExchanges);
 router.get('/sent', authMiddleware, getSentExchanges);
 router.put('/status', authMiddleware, updateExchangeStatus);
 router.get('/:exchangeId', authMiddleware, getExchangeById);
-router.post('/upload-receipt', authMiddleware, upload.single('receipt'), uploadReceipt); // Nueva ruta para cargar comprobantes
+router.post('/upload-receipt', authMiddleware, upload.single('receipt'), uploadReceipt);
+router.get('/all', authMiddleware, getAllExchanges);
+router.get('/completed', authMiddleware, getCompletedExchanges);
 
 module.exports = router;
