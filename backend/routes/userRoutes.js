90f23f72f3be2fef401cb15a82d47abf9d502022
Jinson25 <am987406@gmail.com>
2024-08-07 19:17:54 -0500

    update profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const authMiddleware = require('../middleware/authMiddleware');
+const { updateProfile } = require('../controllers/userController');
 
 // Ruta para obtener la información de un usuario por su ID
 router.get('/:userId', userController.getUserById);
@@ -14,6 +15,8 @@ router.post('/:userId/unfollow', authMiddleware, userController.unfollowUser);
 
 // Ruta para dar like a un usuario
 router.post('/:userId/like', authMiddleware, userController.likeUser);
+
+router.put("/profile", authMiddleware, updateProfile);
 
 // Ruta para actualizar el perfil del usuario autenticado
 //router.put('/profile', authMiddleware, userController.updateProfile);
